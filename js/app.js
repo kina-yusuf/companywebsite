7aa6d3f7d9f42a0d823f1b25b62fbac88a100c73
yusuf kina <yusufkina61@gmail.com>
2024-05-21 23:39:05 +0300

    link, css ve js düzenlemeleri yapıldı. Kontrol edildi.

--- js/app.js
+++ js/app.js
@@ -11,8 +11,7 @@ $(document).ready(function(){
     });
 });
 
-
-/* Man slider */ 
+/* project slider */ 
 
 $(document).ready(function(){
     $('.slider-product').slick({
@@ -74,7 +73,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const totalPages = Math.ceil(filteredProjects.length / itemsPerPage);
 
     const firstButton = document.createElement('button');
-    firstButton.textContent = 'İlk Sayfa';
+    firstButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-left" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+    <path fill-rule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+    </svg>`;
     firstButton.disabled = currentPage === 1;
     firstButton.addEventListener('click', () => {
       currentPage = 1;
@@ -83,7 +85,10 @@ document.addEventListener('DOMContentLoaded', () => {
     paginationContainer.appendChild(firstButton);
 
     const prevButton = document.createElement('button');
-    prevButton.textContent = 'Önceki';
+    prevButton.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+    </svg>`;
     prevButton.disabled = currentPage === 1;
     prevButton.addEventListener('click', () => {
       if (currentPage > 1) {
@@ -107,7 +112,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     const nextButton = document.createElement('button');
-    nextButton.textContent = 'Sonraki';
+    nextButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+    </svg>`;
     nextButton.disabled = currentPage === totalPages;
     nextButton.addEventListener('click', () => {
       if (currentPage < totalPages) {
@@ -118,7 +125,10 @@ document.addEventListener('DOMContentLoaded', () => {
     paginationContainer.appendChild(nextButton);
 
     const lastButton = document.createElement('button');
-    lastButton.textContent = 'Son Sayfa';
+    lastButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-right" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708"/>
+    <path fill-rule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708"/>
+  </svg>`;
     lastButton.disabled = currentPage === totalPages;
     lastButton.addEventListener('click', () => {
       currentPage = totalPages;
@@ -147,7 +157,6 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-
   // project details
 document.addEventListener('DOMContentLoaded', () => {
     const urlParams = new URLSearchParams(window.location.search);
@@ -158,11 +167,13 @@ document.addEventListener('DOMContentLoaded', () => {
     if (project) {
       const projectDetailsContainer = document.getElementById('project-details');
       projectDetailsContainer.innerHTML = `
-        <h2>${project.name}</h2>
-        <p>${project.details}</p>
+        <img src="${project.image}" class="img-detail">
+        <div class="container">
+        <h2 class= "name-detail">${project.name}</h2>
+        <p class="text-detail">${project.details}</p>
+        </div>
       `;
     } else {
       document.getElementById('project-details').innerHTML = '<p>Proje bulunamadı.</p>';
     }
-  });
-  
+});
